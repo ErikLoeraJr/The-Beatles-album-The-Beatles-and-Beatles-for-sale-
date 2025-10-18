ab0591378f2aef0526a66925c4a53e14f7bdae68
ErikLoeraJr <42202367@gmail.com>
2025-10-17 21:30:27 -0600

    Actualizacion de enlaces

--- albums.js
+++ albums.js
@@ -7,103 +7,103 @@ const albums = [
         canciones: [
             {
                 nombre: "Back in the U.S.S.R.",
-                url: "https://www.youtube.com/watch?v=nS5_EQgbuLc",
+                url: "https://www.youtube.com/embed/nS5_EQgbuLc&list=RDnS5_EQgbuLc&start_radio=1",
                 duracion: "2:43",
                 compositores: "Lennon, McCartney"
             },
             {
                 nombre: "Dear Prudence",
-                url: "https://www.youtube.com/watch?v=wQA59IkCF5I&list=RDwQA59IkCF5I&start_radio=1",
+                url: "https://www.youtube.com/embed/wQA59IkCF5I&list=RDwQA59IkCF5I&start_radio=1",
                 duracion: "3:56",
                 compositores: "Lennon, McCartney"
             },
             {
                 nombre: "Glass Onion",
-                url: "https://www.youtube.com/watch?v=jGhcsuqNIOc&list=RDjGhcsuqNIOc&start_radio=1",
+                url: "https://www.youtube.com/embed/jGhcsuqNIOc&list=RDjGhcsuqNIOc&start_radio=1",
                 duracion: "2:18",
                 compositores: "Lennon, McCartney"
             },
             {
                 nombre: "Ob-La-Di, Ob-La-Da",
-                url: "https://www.youtube.com/watch?v=_J9NpHKrKMw&list=RD_J9NpHKrKMw&start_radio=1",
+                url: "https://www.youtube.com/embed/_J9NpHKrKMw&list=RD_J9NpHKrKMw&start_radio=1",
                 duracion: "3:09",
                 compositores: "Lennon, McCartney"
             },
             {
                 nombre: "Wild Honey Pie",
-                url: "https://www.youtube.com/watch?v=l-ekNlk5VDM&list=RDl-ekNlk5VDM&start_radio=1",
+                url: "https://www.youtube.com/embed/l-ekNlk5VDM&list=RDl-ekNlk5VDM&start_radio=1",
                 duracion: "0:52",
                 compositores: "Harrison"
             },
             {
                 nombre: "The continuing story of bungalow bill",
-                url: "https://www.youtube.com/watch?v=J39DC9t0I5o&list=RDJ39DC9t0I5o&start_radio=1",
+                url: "https://www.youtube.com/embed/J39DC9t0I5o&list=RDJ39DC9t0I5o&start_radio=1",
                 duracion: "3:14",
                 compositores: "Harrison"
             },
             {
                 nombre: "While My Guitar Gently Weeps",
-                url: "https://www.youtube.com/watch?v=VJDJs9dumZI&list=RDVJDJs9dumZI&start_radio=1",
+                url: "https://www.youtube.com/embed/VJDJs9dumZI&list=RDVJDJs9dumZI&start_radio=1",
                 duracion: "4:45",
                 compositores: "Harrison"
             },
             {
                 nombre: "Happiness is a Warm Gun",
-                url: "Happiness is a Warm Gun",
+                url: "https://www.youtube.com/embed/vdvnOH060Qg&list=RDvdvnOH060Qg&start_radio=1",
                 duracion: "2:44",
                 compositores: "Harrison"
             },
             {
                 nombre: "Martha My Dear",
-                url: "https://www.youtube.com/watch?v=RXawa90YU2s&list=RDRXawa90YU2s&start_radio=1",
+                url: "https://www.youtube.com/embed/RXawa90YU2s&list=RDRXawa90YU2s&start_radio=1",
                 duracion: "2:28",
                 compositores: "Harrison"
             },
             {
                 nombre: "I am so tired",
-                url: "https://www.youtube.com/watch?v=7cqHtGb9WYM&list=RD7cqHtGb9WYM&start_radio=1",
+                url: "https://www.youtube.com/embed/7cqHtGb9WYM&list=RD7cqHtGb9WYM&start_radio=1",
                 duracion: "2:03",
                 compositores: "Harrison"
             },
             {
                 nombre: "Blackbird",
-                url: "https://www.youtube.com/watch?v=Man4Xw8Xypo&list=RDMan4Xw8Xypo&start_radio=1",
+                url: "https://www.youtube.com/embed/Man4Xw8Xypo&list=RDMan4Xw8Xypo&start_radio=1",
                 duracion: "2:18",
                 compositores: "Harrison"
             },
             {
                 nombre: "Piggies",
-                url: "https://www.youtube.com/watch?v=RhY1x8CpWeI&list=RDRhY1x8CpWeI&start_radio=1",
+                url: "https://www.youtube.com/embed/RhY1x8CpWeI&list=RDRhY1x8CpWeI&start_radio=1",
                 duracion: "2:04",
                 compositores: "Harrison"
             },
             {
                 nombre: "WRocky Racoon",
-                url: "https://www.youtube.com/watch?v=sDcDCZGcZj8&list=RDsDcDCZGcZj8&start_radio=1",
+                url: "https://www.youtube.com/embed/sDcDCZGcZj8&list=RDsDcDCZGcZj8&start_radio=1",
                 duracion: "3:33",
                 compositores: "Harrison"
             },
             {
                 nombre: "Dont pass me by",
-                url: "https://www.youtube.com/watch?v=PTKEiQHHsuk&list=RDPTKEiQHHsuk&start_radio=1",
+                url: "https://www.youtube.com/embed/PTKEiQHHsuk&list=RDPTKEiQHHsuk&start_radio=1",
                 duracion: "3:50",
                 compositores: "Harrison"
             },
             {
                 nombre: "Why dont we do it in the road?",
-                url: "https://www.youtube.com/watch?v=p4E6KtQg_z0&list=RDp4E6KtQg_z0&start_radio=1I",
+                url: "https://www.youtube.com/embed/p4E6KtQg_z0&list=RDp4E6KtQg_z0&start_radio=1I",
                 duracion: "1:41",
                 compositores: "Harrison"
             },
             {
                 nombre: "I will",
-                url: "https://www.youtube.com/watch?v=p-abNGP1BK4&list=RDp-abNGP1BK4&start_radio=1",
+                url: "https://www.youtube.com/embed/p-abNGP1BK4&list=RDp-abNGP1BK4&start_radio=1",
                 duracion: "1:45",
                 compositores: "Harrison"
             },
             {
                 nombre: "Julia",
-                url: "https://www.youtube.com/watch?v=TZip_br_v3w&list=RDTZip_br_v3w&start_radio=1",
+                url: "https://www.youtube.com/embed/TZip_br_v3w&list=RDTZip_br_v3w&start_radio=1",
                 duracion: "2:54",
                 compositores: "Harrison"
             }
@@ -117,85 +117,85 @@ const albums = [
         canciones: [
             {
                 nombre: "No Reply",
-                url: "https://www.youtube.com/watch?v=YgFo9STa70E&list=RDYgFo9STa70E&start_radio=1",
+                url: "https://www.youtube.com/embed/YgFo9STa70E&list=RDYgFo9STa70E&start_radio=1",
                 duracion: "2:15",
                 compositores: "Lennon, McCartney"
             },
             {
                 nombre: "I'm a Loser",
-                url: "https://www.youtube.com/watch?v=f70Z3cvrQd0&list=RDf70Z3cvrQd0&start_radio=1",
+                url: "https://www.youtube.com/embed/f70Z3cvrQd0&list=RDf70Z3cvrQd0&start_radio=1",
                 duracion: "2:31",
                 compositores: "Lennon, McCartney"
             },
             {
                 nombre: "Baby's in Black",
-                url: "https://www.youtube.com/watch?v=TJFpUb7JGYo&list=RDTJFpUb7JGYo&start_radio=1",
+                url: "https://www.youtube.com/embed/TJFpUb7JGYo&list=RDTJFpUb7JGYo&start_radio=1",
                 duracion: "2:02",
                 compositores: "Lennon, McCartney"
             },
             {
                 nombre: "Rock and Roll Music",
-                url: "https://www.youtube.com/watch?v=IRF6nmqcbxo&list=RDIRF6nmqcbxo&start_radio=1",
+                url: "https://www.youtube.com/embed/IRF6nmqcbxo&list=RDIRF6nmqcbxo&start_radio=1",
                 duracion: "2:32",
                 compositores: "Berry"
             },
             {
                 nombre: "I'll Follow the Sun",
-                url: "https://www.youtube.com/watch?v=28d_A_NuJ7A&list=RD28d_A_NuJ7A&start_radio=1",
+                url: "https://www.youtube.com/embed/28d_A_NuJ7A&list=RD28d_A_NuJ7A&start_radio=1",
                 duracion: "1:48",
                 compositores: "Lennon, McCartney"
             },
             {
                 nombre: "Mr. Moonlight",
-                url: "https://www.youtube.com/watch?v=FZQ8nWZJrhA&list=RDFZQ8nWZJrhA&start_radio=1",
+                url: "https://www.youtube.com/embed/FZQ8nWZJrhA&list=RDFZQ8nWZJrhA&start_radio=1",
                 duracion: "1:38",
                 compositores: "Lennon, McCartney"
             },
              {
                 nombre: "Kansas City/Hey-Hey-Hey-Hey!",
-                url: "https://www.youtube.com/watch?v=4xvW-Nf9VAw&list=RD4xvW-Nf9VAw&start_radio=1",
+                url: "https://www.youtube.com/embed/4xvW-Nf9VAw&list=RD4xvW-Nf9VAw&start_radio=1",
                 duracion: "2:33",
                 compositores: "Leiber/Stoller/Penniman"
             },
             {
                 nombre: "Eight Days a Week",
-                url: "https://www.youtube.com/watch?v=kle2xHhRHg4&list=RDkle2xHhRHg4&start_radio=1",
+                url: "https://www.youtube.com/embed/kle2xHhRHg4&list=RDkle2xHhRHg4&start_radio=1",
                 duracion: "2:44",
                 compositores: "Lennon, McCartney"
             },
             {
                 nombre: "Words of Love",
-                url: "https://www.youtube.com/watch?v=no-nuFTHetE&list=RDno-nuFTHetE&start_radio=1",
+                url: "https://www.youtube.com/embed/no-nuFTHetE&list=RDno-nuFTHetE&start_radio=1",
                 duracion: "2:12",
                 compositores: "Holly"
             },
             {
                 nombre: "Honey Don't",
-                url: "https://www.youtube.com/watch?v=XIc-VCw5r0A&list=RDXIc-VCw5r0A&start_radio=1",
+                url: "https://www.youtube.com/embed/XIc-VCw5r0A&list=RDXIc-VCw5r0A&start_radio=1",
                 duracion: "2:56",
                 compositores: "Perkins"
             },
             {
                 nombre: "Every Little Thing",
-                url: "https://www.youtube.com/watch?v=jrDikOz8YSU&list=RDjrDikOz8YSU&start_radio=1",
+                url: "https://www.youtube.com/embed/jrDikOz8YSU&list=RDjrDikOz8YSU&start_radio=1",
                 duracion: "2:01",
                 compositores: "Lennon, McCartney"
             },
             {
                 nombre: "I Don't Want to Spoil the Party",
-                url: "https://www.youtube.com/watch?v=zqVDvLDLsjI&list=RDzqVDvLDLsjI&start_radio=1",
+                url: "https://www.youtube.com/embed/zqVDvLDLsjI&list=RDzqVDvLDLsjI&start_radio=1",
                 duracion: "2:33",
                 compositores: "Lennon, McCartney"
             },
             {
                 nombre: "What You're Doing",
-                url: "https://www.youtube.com/watch?v=7Sba2LbhkVY&list=RD7Sba2LbhkVY&start_radio=1",
+                url: "https://www.youtube.com/embed/7Sba2LbhkVY&list=RD7Sba2LbhkVY&start_radio=1",
                 duracion: "2:30",
                 compositores: "Lennon, McCartney"
             },
             {
                 nombre: "Everybody's Trying to Be My Baby",
-                url: "https://www.youtube.com/watch?v=wn7FiAdHcmw&list=RDwn7FiAdHcmw&start_radio=1",
+                url: "https://www.youtube.com/embed/wn7FiAdHcmw&list=RDwn7FiAdHcmw&start_radio=1",
                 duracion: "2:24",
                 compositores: "Perkins"
             }
